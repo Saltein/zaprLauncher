c8b888a8a24c14a56ec30504c11743af64dbdd56
Никита <nikitval@list.ru>
2025-12-20 16:25:03 +0300

    Запрет масштабирования

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,30 @@
 import './App.css';
 import icon from '../../build/windows/icon.ico'
 import { Loader } from './shared/Loader/Loader';
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+        const onWheel = (e: WheelEvent) => {
+            if (e.ctrlKey) {
+                e.preventDefault();
+            }
+        };
+
+        const onKeyDown = (e: KeyboardEvent) => {
+            if (e.ctrlKey && ['+', '-', '=', '0'].includes(e.key)) {
+                e.preventDefault();
+            }
+        };
+
+        window.addEventListener('wheel', onWheel, { passive: false });
+        window.addEventListener('keydown', onKeyDown);
+
+        return () => {
+            window.removeEventListener('wheel', onWheel);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, []);
 
 
     return (
